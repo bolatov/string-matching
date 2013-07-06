45012560abe587c302a9e29590435f4521024747
Almer Bolatov <almer.bolatov@gmail.com>
2013-07-06 16:04:51 +0200

    Add strings to the trie with id. Looks up strings in the trie.

--- src/de/saarland/Main.java
+++ src/de/saarland/Main.java
@@ -3,7 +3,19 @@ package de.saarland;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello world");
+
+        Trie trie = new Trie();
+        trie.add("aba", 0);
+        trie.add("bba", 1);
+
+        String pattern = "aba";
+        int id[] = trie.lookUp(pattern);
+        if (id.length < 1) {
+            System.out.println(String.format("Pattern '%s' is not found in the dictionary.%n", pattern));
+        } else {
+            System.out.println(String.format("Pattern '%s' is found", pattern));
+        }
     }
 }
